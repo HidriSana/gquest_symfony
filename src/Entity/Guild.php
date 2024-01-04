b1197a985f780c7a87675783d359ad99f7e17d47
azaiezsene@yahoo.fr <azaiezsene@yahoo.fr>
2024-01-04 13:53:04 +0100

    Created Entity User and Guild

---
+++ src/Entity/Guild.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GuildRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GuildRepository::class)]
+class Guild
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $guild_name = null;
+
+    #[ORM\OneToMany(mappedBy: 'guild', targetEntity: User::class)]
+    private Collection $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGuildName(): ?string
+    {
+        return $this->guild_name;
+    }
+
+    public function setGuildName(string $guild_name): static
+    {
+        $this->guild_name = $guild_name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): static
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+            $user->setGuild($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): static
+    {
+        if ($this->users->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getGuild() === $this) {
+                $user->setGuild(null);
+            }
+        }
+
+        return $this;
+    }
+}
